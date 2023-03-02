3c30f790745a4218207bacdcca8278210842e073
Tristan <74349933+Socuz@users.noreply.github.com>
2023-03-02 11:51:14 +0200

    initial commit, WIP parse html file

--- brokenExtLinks.py
+++ brokenExtLinks.py
@@ -1,35 +1,13 @@
-import subprocess
-import csv
+from bs4 import BeautifulSoup
 
-def check_broken_ext_links():
-    headerList = ["urlname", "parentname", "base", "result", "warningstring", "infostring", "valid", "url", "line", "column","name", "dltime", "size", "checktime", "cached", "level", "modified"]
+with open("linkchecker-out.html", "r") as htmlfile:
+    contents = htmlfile.read()
+    soup = BeautifulSoup(contents, 'html.parser')
 
-    subprocess.run(["linkchecker", "--check-extern", "-F", "csv", "-q", "https://docs.csc.fi"])
+    for name in soup:
+        if soup.tag.name('URL'):
+            print(name)
+    #print(soup.get_text())
 
-    # Clean csv linkchecker output
-    with open("linkchecker-out.csv", "r") as csv_in, open("broken_links.csv", "w") as csv_out:
-        csv_writer = csv.writer(csv_out)
-        dw = csv.DictWriter(csv_out, delimiter=';', fieldnames=headerList)
-        dw.writeheader()
-
-        [csv_writer.writerow(row) for row in csv.reader(csv_in) if row[0].startswith("http")]
-
-    # Read the new output file and get 404 errors
-    with open("broken_links.csv", "r") as csv_in:
-        csv_reader = csv.reader(csv_in, delimiter=';')
-        line_count = 0
-
-        print("Broken links:")
-        for row in csv_reader:
-            try:
-                error = row[3].startswith("404")
-                if error:
-                    print(f"PARENT URL: {row[1]} has this broken link URL: {row[0]}")
-                    line_count += 1
-            except IndexError:
-                pass
-        
-        print(f"Processed: {line_count}")
-
-if __name__ == "__main__":
-    check_broken_ext_links()
+    # for tag in soup.find_all('td'):
+    #     print(tag.name)
