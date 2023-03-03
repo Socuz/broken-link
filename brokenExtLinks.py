7aeb6a41bfb668d1b3f16b1742b896fcffe5ac68
Tristan <74349933+Socuz@users.noreply.github.com>
2023-03-03 11:56:24 +0200

    revamped and parse text file

--- brokenExtLinks.py
+++ brokenExtLinks.py
@@ -1,13 +1,13 @@
-from bs4 import BeautifulSoup
+with open("linkchecker-out.txt", "r") as f_in:
+    file = f_in.read()
+    result = "Result     Error: 404 Not Found"
+    counter = 0
 
-with open("linkchecker-out.html", "r") as htmlfile:
-    contents = htmlfile.read()
-    soup = BeautifulSoup(contents, 'html.parser')
-
-    for name in soup:
-        if soup.tag.name('URL'):
-            print(name)
-    #print(soup.get_text())
-
-    # for tag in soup.find_all('td'):
-    #     print(tag.name)
+    for line in file.split('\n\n'):
+        if result in line:
+            url = line.split('\n')[0].split('        ')[-1]
+            name = line.split('\n')[1].split('       ')[-1]
+            parent_url = line.split('\n')[2].split(' ')[-5].strip(',')
+            counter += 1
+            print(f"URL: {url}\nName: {name}\nParent URL: {parent_url}\n{result}\n")
+    print(f"Processed: {counter}")
