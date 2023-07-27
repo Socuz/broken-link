4032c588a4d4bfaa19e99ae4234dfa69a1ec3c1c
Tristan <74349933+Socuz@users.noreply.github.com>
2023-07-27 13:28:50 +0300

    add send report by email

--- app.py
+++ app.py
@@ -1,31 +1,18 @@
-#!/usr/bin/python3
-"""
-Module for checking broken links (404)
-
-Use https://docs.csc.fi/ as default URL for checking. 
-It will last around 10 minutes for https://docs.csc.fi/
-
-If you want to test another URL, you need to specify as argument. 
-Don't forget to add http or https at the beginning.
-
-eg: ./broken_ext_links.py https://www.google.com
-"""
-
 import subprocess
-import sys
-import re
+import smtplib
+from email.message import EmailMessage
 
-def check_broken_ext_links(url):
-    """
-    Function for checking broken links (404).
-    """
-    subprocess.run(["linkchecker", "--check-extern", "--user-agent", "Mozilla/5.0 (compatible; LinkChecker/9.3; +http://wummel.github.io/linkchecker/)", "-F", "text", "-q", url], check=False)
+FILENAME = "linkchecker-out.txt"
+OUTPUTFILE = "404.txt"
+URL = "https://docs.csc.fi/"
+subprocess.run(["linkchecker", "--check-extern", "--user-agent", "Mozilla/5.0 (compatible; LinkChecker/9.3; +http://wummel.github.io/linkchecker/)", "-F", "text", "-q", URL], check=False)
 
-    with open("linkchecker-out.txt", "r", encoding="utf-8") as f_in:
-        file = f_in.read()
-        result = "Result     Error: 404 Not Found"
-        counter = 0
+with open(FILENAME, "r", encoding="utf-8") as f_in:
+    file = f_in.read()
+    result = "Result     Error: 404 Not Found"
+    counter = 0
 
+    with open("404.txt", "w+", encoding="utf-8") as f_out:
         print("BROKEN LINKS REPORT: \n")
         for line in file.split('\n\n'):
             if result in line:
@@ -34,19 +21,17 @@ def check_broken_ext_links(url):
                 parent_url = line.split('\n')[2].split(' ')[-5].strip(',')
                 counter += 1
                 print(f"URL: {url}\nName: {name}\nParent URL: {parent_url}\n{result}\n")
+                f_out.write(f"URL: {url}\nName: {name}\nParent URL: {parent_url}\n{result}\n\n")
         print(f"Processed: {counter}")
+        f_out.write(f"Processed: {counter}")
 
-if __name__ == "__main__":
-    r = re.compile("http(s?)://.+")
+msg = EmailMessage()
+msg["From"] = "noreply@csc.fi"
+msg["Subject"] = "Broken links report"
+msg["To"] = "tristan.perard@csc.fi"
+msg.set_content("You will find the report of broken link from docs.csc.fi")
+msg.add_attachment(open(OUTPUTFILE, "r", encoding="utf-8").read())
 
-    if len(sys.argv) != 2:
-        URL = "https://docs.csc.fi/"
-        print(f"No URL specified, testing: {URL}")
-        check_broken_ext_links(URL)
-    else:
-        URL = sys.argv[1]
-        if r.match(URL):
-            print(f"Testing {URL}")
-            check_broken_ext_links(URL)
-        else:
-            print("Incorrect format of the URL, don't forget to add http or https at the beginning")
+s = smtplib.SMTP('smtp.pouta.csc.fi')
+s.send_message(msg)
+print("Email sent")
